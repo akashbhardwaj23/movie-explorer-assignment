36f1d2074c5d9e742c36ac061f0537f2aa25180d
akashbhardwaj23 <akashbhardwaj415@gmail.com>
2025-08-02 07:38:54 +0100

    added auth middleware and types

---
+++ src/middleware/auth.ts
@@ -0,0 +1,27 @@
+import type { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+
+
+export function authMiddleware(req: Request, res : Response, next : NextFunction) {
+
+    const authorizationHeader = req.headers.authorization;
+    if (!authorizationHeader) {
+    return res.status(401).json({ error: "Authorization header is missing" });
+  }
+    const token = authorizationHeader.split(" ")[1];
+    if (!token) {
+        return res.status(401).json({ error: "Token is missing from the authorization header" });
+    }
+
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET as string)
+
+    if(typeof decodedToken === "string" || !decodedToken || !decodedToken.sub || !decodedToken.sub) {
+        return res.status(401).json({ error: "Invalid token" });
+    }
+
+    req.userId = decodedToken.sub
+
+    next()
+
+}
